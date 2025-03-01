d8b5e84c3915206f841faafce05613e92cda13f3
imorton <imorton93@live.ca>
2025-02-28 20:10:55 -0800

    The postal code input is now a sidebar menu instead of a dropdown

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,67 +1,57 @@
 import { useEffect, useState } from "react";
-import { Navbar, Nav, Form, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Form, NavDropdown, Button, Offcanvas } from "react-bootstrap";
 import './Navbar.css'
 import { useUserLocation } from "../../context/UserLocationProvider";
 
 function NavBar(){
-    const [showDropDown, setShowDropDown] = useState(false);
-    const [dropDownTitle, setDropDownTitle] = useState('Enter Postal Code')
+    const [title, setTitle] = useState('Enter Postal Code')
     const { geoError, postalCode, setPostalCode, source, loading } = useUserLocation();
+    const [showSidebar ,setShowSidebar] = useState(false);
 
     useEffect(() => {
         if(source === 'postalCode'){
-            setDropDownTitle('Postal Code Set!');
+            setTitle('Postal Code Set!');
         } else{
-            setDropDownTitle('Enter Postal Code');
+            setTitle('Enter Postal Code');
         }
     }, [source])
 
 
     return (
-        <Navbar bg="dark" expand="lg" className="p-2">
+
+        <>
+            <Navbar bg="dark" expand="lg" className="p-2">
             <Navbar.Brand href="#" className="text-white">Vancity Happy Hour</Navbar.Brand>
-            
-            <Nav className="ms-auto">
-                {/* Dropdown for Postal Code */}
-                <NavDropdown 
-                    title={dropDownTitle}
-                    align="end" 
-                    className="custom-dropdown" 
-                    show={showDropDown}
-                    onClick={() => setShowDropDown(true)}
-                    onMouseEnter={() => setShowDropDown(true)}
-                    onMouseLeave={() => setShowDropDown(false)}
-                    >
-                    <NavDropdown.Item 
-                        style={{ minWidth: "250px", padding: "10px" }} 
-                        className="custom-dropdown-item"
-                        onClick={(e) => e.stopPropagation()} //prevents default behavior
-                        >
-                        <Form.Group controlId="postalCodeInput">
-                            <Form.Label style={{ fontSize: "0.85rem" }} className="custom-dropdown-label">Enter Postal Code:</Form.Label>
-                            <Form.Control
-                                size="sm"
-                                type="text"
-                                placeholder="A1A1A1"
-                                value={postalCode}
-                                maxLength={6}
-                                onChange={(e) => setPostalCode(e.target.value)}
-                                style={{ fontSize: "0.85rem", padding: "5px" }}
-                                className="custom-dropdown-input"
-                                onClick={(e) => e.stopPropagation()}
-                            />
-                        </Form.Group>
-                        {geoError && <p style={{ color: "red", fontSize: "0.8rem" }}>{geoError}</p>}
+                <Nav className="ms-auto">
+                   <Nav.Link className="text-white" onClick={() => setShowSidebar(true)}>{title}</Nav.Link>
+                </Nav>
+
+                <Offcanvas show={showSidebar} onHide={() => setShowSidebar(false)} placement="end" className="custom-sidebar">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title className="custom-sidebar-title">Enter Postal Code</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                    <Form.Group controlId="postalCodeInput">
+                        <Form.Label className="custom-sidebar-label">{title}</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="A1A1A1"
+                            value={postalCode}
+                            maxLength={6}
+                            onChange={(e) => setPostalCode(e.target.value)}
+                            className="custom-sidebar-input"
+                        />
+                    </Form.Group>
+                        {geoError && <p style={{ color: "red" }}>{geoError}</p>}
                         {loading && (
                             <p style={{ color: "blue" }}>
                                 Loading<span className="dots"></span>
                             </p>
                         )}
-                    </NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            
-        </Navbar>
+                    </Offcanvas.Body>
+                </Offcanvas>
+            </Navbar>
+        </>
     );
 };
 
